f9f23b8bc933c9a2876e835bc152427d919e26b6
SMTareq <tareqcreatrix@gmail.com>
2024-05-09 18:01:55 +0600

    TryDemo

---
+++ GAMEPORTALCMS/Controllers/TextDemo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GAMEPORTALCMS.Controllers
+{
+    public class TextDemo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
